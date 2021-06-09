7757a85dac86705cc0a479cfeb0a2e06093d4f0f
James Jefferson <jrj72@Jamess-Mac-mini.local>
2021-06-08 20:50:58 -0500

    score board working

--- pong.py
+++ pong.py
@@ -6,6 +6,11 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -36,14 +41,15 @@ ball.goto(0, 0)
 ball.dx = 0.1
 ball.dy = -0.1
 
-#Pen
-pen  = turtle.Turtle()
+# Pen
+pen = turtle.Turtle()
 pen.speed(0)
 pen.color("white")
 pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
-pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal") )
+pen.write("Player A:0 Player B:0", align="center",
+          font=("Courier", 24, "normal"))
 
 # Function
 
@@ -101,17 +107,24 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b),
+                  align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b),
+          align="center", font=("Courier", 24, "normal"))
 
-    # Paddle and Ball collisions
-    if (ball.xcor() > 340 and ball.ycor() < 350) and (ball.ycor() <paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
+ # Paddle and Ball collisions
+    if (ball.xcor() > 340 and ball.ycor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
-         
-    if (ball.xcor() < -340 and ball.ycor() >  -350) and (ball.ycor() <paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() -40):
+
+    if (ball.xcor() < -340 and ball.ycor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
-    
